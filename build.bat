f183f1e82712a45331b312fb1f076aa3b0504bf2
Hüseyin <iletisim@huseyinyigit.com.tr>
2020-11-05 14:52:08 +0300

    Update build file

--- build.bat
+++ build.bat
@@ -1,1 +1,7 @@
-cl /EHsc main.cpp lib\disphelper.lib user32.lib
+@ECHO OFF
+ECHO # Compiling ...
+cl /nologo /EHsc /Idisphelper_081\single_file_source main.cpp disphelper_081\single_file_source\disphelper.lib user32.lib /Fe:main.exe
+ECHO Finished. 
+ECHO # Cleaning ...
+DEL main.obj main.pdb vc140.pdb
+ECHO Finished. 
